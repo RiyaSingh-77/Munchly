9aa2c1a9eeb82d87a534d92054436e015c6a3acf
RiyaSingh-77 <151558616+RiyaSingh-77@users.noreply.github.com>
2026-03-10 02:32:43 +0530

    foodPartner auth done

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -5,9 +5,18 @@ const authController = require("../controllers/auth.controller")
 const router = express.Router();
 //Creation of API
 
+//User auth APIs
 router.post('/user/register', authController.registerUser)
 router.post('/user/login', authController.loginUser)
 router.get('/user/logout', authController.logoutUser)
+
+
+//Food partner auth APIs
+
+router.post('/food-partner/register', authController.registerFoodPartner)
+router.post('/food-partner/login', authController.loginFoodPartner)
+router.get('/food-partner/logout', authController.logoutFoodPartner)
+
 
 
 
