754ea92fdcd0592484cc3aad938c93819ecfb6f0
RiyaSingh-77 <151558616+RiyaSingh-77@users.noreply.github.com>
2026-03-15 02:18:02 +0530

    Added frontend UI of the app with authentication pages

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import AppRoutes from './routes/AppRoutes'
+
+import './styles/theme.css'
+import './styles/variables.css'
+import './styles/auth-shared.css'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <AppRoutes /> 
+    </>
+  )
+}
+
+export default App
---
+++ Frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+
+import UserLogin from '../pages/UserLogin'
+import UserRegister from '../pages/UserRegister'
+import FoodPartnerLogin from '../pages/FoodPartnerLogin'
+import FoodPartnerRegister from '../pages/FoodPartnerRegister'
+
+
+
+const AppRoutes = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/user/login" replace />} />
+        <Route path="/user/register" element={<UserRegister />} />
+        <Route path="/user/login" element={<UserLogin />} />
+        <Route path="/food-partner/register" element={<FoodPartnerRegister />} />
+        <Route path="/food-partner/login" element={<FoodPartnerLogin />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+
+      
+      </Routes>
+    </Router>
+  )
+}
+
+export default AppRoutes
