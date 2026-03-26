3594aece5dae6099f42e8bf8bff3a039d4aab8f3
RiyaSingh-77 <151558616+RiyaSingh-77@users.noreply.github.com>
2026-03-26 16:10:30 +0530

    Fixed Routes

--- Frontend/src/routes/AppRoutes.jsx
+++ Frontend/src/routes/AppRoutes.jsx
@@ -1,42 +1,23 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import RoleSelect from '../pages/RoleSelect'
-import UserLogin from '../pages/UserLogin'
-import UserRegister from '../pages/UserRegister'
-import FoodPartnerLogin from '../pages/FoodPartnerLogin'
-import FoodPartnerRegister from '../pages/FoodPartnerRegister'
-import Home from '../pages/general/Home'; 
-import CreateFood from '../pages/food-partner/CreateFood';
-import Profile from '../pages/food-partner/Profile'
-import ReelFeed from '../components/ReelFeed'
-import Saved from '../pages/general/Saved'
-import Dashboard from '../pages/food-partner/Dashboard'
-
-
-
-
-
-
 const AppRoutes = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/home" element= {<Home />} />
+        <Route path="/" element={<RoleSelect />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/user/register" element={<UserRegister />} />
         <Route path="/user/login" element={<UserLogin />} />
         <Route path="/food-partner/register" element={<FoodPartnerRegister />} />
         <Route path="/food-partner/login" element={<FoodPartnerLogin />} />
-        <Route path="/" element={<RoleSelect />} />
-        <Route path="*" element={<Navigate to="/home" replace />} />
-        <Route path = "/create-food" element={<CreateFood />} />
-        <Route path="/food-partner/profile/:id" element={<Profile />} />
-        <Route path="/feed" element={<ReelFeed />} /> 
-        <Route path="/saved" element={<Saved />} />
         <Route path="/food-partner/dashboard" element={<Dashboard />} />
+        <Route path="/food-partner/create-food" element={<CreateFood />} />  {/* fix path too */}
+        <Route path="/food-partner/profile/:id" element={<Profile />} />
+        <Route path="/feed" element={<ReelFeed />} />
+        <Route path="/saved" element={<Saved />} />
 
+        <Route path="*" element={<Navigate to="/home" replace />} />
       </Routes>
     </Router>
   )
 }
 
-export default AppRoutes
+export default AppRoutes;
